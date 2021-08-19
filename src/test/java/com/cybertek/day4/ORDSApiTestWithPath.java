65b2b75115f628471577a8145b4c892b6d9797da
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-08-18 19:04:39 -0500

    added Day4

---
+++ src/test/java/com/cybertek/day4/ORDSApiTestWithPath.java
@@ -0,0 +1,48 @@
+package com.cybertek.day4;
+
+import com.cybertek.utilities.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ORDSApiTestWithPath extends HrTestBase {
+
+    @DisplayName("GET request to countries with Path method")
+    @Test
+    public void test1() {
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("q", "{\"region_id\":2}")
+                .when()
+                .get("/countries");
+        assertEquals(200, response.statusCode());
+
+        //print limit result
+        System.out.println("response.path(\"limit\") = " +response.path("limit"));
+
+        //print hasMore
+        System.out.println("response.path(\"hasMore\") = " + response.path("hasMore"));
+
+        //print first CountryId
+        String firstCountryId = response.path("items[0].country_id");
+        System.out.println("firstCountryId = " + firstCountryId);
+
+        //print second country name
+        String secondCountryName = response.path("items[1].country_name");
+        System.out.println("secondCountryId = " +secondCountryName);
+        //print "http://52.207.61.129:1000/ords/hr/countries/CA"
+        String thirdHref = response.path("items[2].links[0].href");
+        System.out.println("thirdHref = " + thirdHref);
+
+        //get me all country names
+        List<String> countryNames = response.path("items.country_name");
+        System.out.println("countryNames = " + countryNames);
+
+    }
+}
