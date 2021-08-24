e5a4c0d6aa5d800e9c4d07ba1e78ec8983073206
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-08-24 15:42:47 -0500

    Day6 API

--- src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -1,6 +1,10 @@
 package com.cybertek.day6;
 
+import com.cybertek.pojo.Link;
+import com.cybertek.pojo.Region;
 import com.cybertek.utilities.HrTestBase;
+import com.cybertek.utilities.HrTestBase;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,6 +16,16 @@ public class ORDSPojoGetRequestTest extends HrTestBase {
 
     @Test
     public void regionTest(){
+
+        JsonPath jsonPath = get("/regions").then().statusCode(200).log().body().extract().jsonPath();
+
+        Region region1 = jsonPath.getObject("items[0]", Region.class);
+
+        System.out.println(region1);
+
+        System.out.println("region1.getRegion_id() = " + region1.getRegion_id());
+        System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
+        System.out.println("region1.getLinks().get(0).getHref() = " + region1.getLinks().get(0).getHref());
 
     }
 }
