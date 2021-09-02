1f3706f81942beabb780487fe4883e86e8f1b4c0
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-09-01 20:52:09 -0500

    Day12 API

--- src/test/java/com/cybertek/utilities/ConfigurationReader.java
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -6,35 +6,22 @@ import java.util.Properties;
 
 public class ConfigurationReader {
 
-        //#1- Create properties object
-        private static Properties properties = new Properties();
+    private static Properties configFile;
 
-        static {
-
-            try {
-                //#2- Load the file into FileInputStream
-                FileInputStream file = new FileInputStream("configuration.properties");
-
-                //#3- load properties object with the file (configuration.properties)
-                properties.load(file);
-
-                //close the file
-                file.close();
-
-            } catch (IOException e) {
-
-                System.out.println("File not found in Configuration properties.");
-
-            }
-
+    static {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("configuration.properties");
+            configFile = new Properties();
+            configFile.load(fileInputStream);
+            fileInputStream.close();
+        } catch (IOException e) {
+            System.out.println("Failed to load properties file!");
+            e.printStackTrace();
         }
+    }
 
-        //USE THE ABOVE CREATED LOGIC TO CREATE A RE-USABLE STATIC METHOD
-        public static String getProperty(String keyWord){
-
-            return properties.getProperty(keyWord);
-        }
-
-
+    public static String getProperty(String key) {
+        return configFile.getProperty(key);
+    }
 
 }
--- src/test/java/com/cybertek/utilities/SpartanNewBase.java
+++ src/test/java/com/cybertek/utilities/SpartanNewBase.java
@@ -1,5 +1,9 @@
 package com.cybertek.utilities;
 
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -7,12 +11,35 @@ import static io.restassured.RestAssured.*;
 
 public class SpartanNewBase {
 
+    public static RequestSpecification requestSpec;
+    public static ResponseSpecification responseSpec;
+    public static RequestSpecification userSpec;
+    public static RequestSpecification adminSpec;
+
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
         baseURI = "http://44.195.19.167";
         port = 7000;
         basePath ="/api";
+
+        requestSpec = given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin", "admin")
+                .log().all();
+
+        userSpec =given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("user", "user")
+                .log().all();
+
+        responseSpec = expect().statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .logDetail(LogDetail.ALL);  //logging with response specficiation
+
     }
 
     @AfterAll
@@ -21,3 +48,4 @@ public class SpartanNewBase {
         reset();
     }
 }
+
